59614b704557d2549f5b87af610e878512dcd338
ramsingh007 <rmsingh157@gmail.com>
2024-06-20 18:00:23 +0800

    Update question3.js
    comment code block

--- question3.js
+++ question3.js
@@ -5,19 +5,19 @@
 * @return {number[]}
 */
 const twoSum = function(numbers, target) {
-    const result = [];
+    const result = [];  // Initialize an empty array to store result indices
     
-    numbers.forEach((num1, index1) => {
-        numbers.slice(index1 + 1).forEach((num2, index2) => {
+    numbers.forEach((num1, index1) => {  // Iterate over each element in the array
+        numbers.slice(index1 + 1).forEach((num2, index2) => {  // Iterate over subsequent elements
             if (num1 + num2 === target && result.length === 0) {
-                result.push(index1 + 1, index1 + index2 + 2);
+                // If the sum of num1 and num2 equals target and result is empty
+                result.push(index1 + 1, index1 + index2 + 2);  // Push indices to result array
             }
         });
     });
     
-    return result;
+    return result;  // Return the result array
 };
-
 console.log("numbers [4, 11, 17, 25] target 21, Answer: ", twoSum([4, 11, 17, 25], 21));
 console.log("numbers [0, 1, 2, 2, 3, 5] target 4  Answer: ",twoSum([0, 1, 2, 2, 3, 5], 4));
 console.log("numbers [-1, 0] target -1  Answer: ",twoSum([-1, 0], -1))
